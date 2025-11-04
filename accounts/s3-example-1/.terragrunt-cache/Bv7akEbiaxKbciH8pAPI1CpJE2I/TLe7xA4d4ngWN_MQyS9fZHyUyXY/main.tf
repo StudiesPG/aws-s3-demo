a2d44f7cd96a7d7b68ea6b2b18882cb4a5b8b90d
Guilherme-Goldmann <guilhermegarguedes@gmail.com>
2025-11-04 12:50:55 -0300

    rm terragrunt-cache dir

--- accounts/s3-example-1/.terragrunt-cache/Bv7akEbiaxKbciH8pAPI1CpJE2I/TLe7xA4d4ngWN_MQyS9fZHyUyXY/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucket_name
-  tags = var.tags
-}
--- accounts/s3-example-1/.terragrunt-cache/Bv7akEbiaxKbciH8pAPI1CpJE2I/TLe7xA4d4ngWN_MQyS9fZHyUyXY/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_id" {
-  description = "The name of the bucket."
-  value       = aws_s3_bucket.mybucket.id
-}
-
-output "s3_bucket_arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = aws_s3_bucket.mybucket.arn
-}
